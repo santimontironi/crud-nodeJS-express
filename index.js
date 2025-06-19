cfa37a53d4fe6b519f5897858cb0a4350f3b590f
santimontironi <santiimontironi@gmail.com>
2025-06-18 21:28:29 -0300

    primeras importaciones realizadas

---
+++ index.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser')
+const pool = require('./db')
+const app = require('express')
+const port = 3000
+
+// se configura el motor para las vistas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// middleware para pasar los datos enviados en un formulario a un objeto JavaScript accesible en req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// servir archivos estáticos
+app.use(express.static('public'));
+
+app.listen(() => {
+    console.log(`Servidor corriendo en el puerto: ${PORT}`);
+})
