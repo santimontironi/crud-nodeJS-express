83ea9458465e967471f100db28987890c45a4955
santimontironi <santiimontironi@gmail.com>
2025-06-21 16:51:09 -0300

    divisiones de plantillas ejs llevadas a cabo, tambien se creo la tabla usuarios desde PostgreSQL

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/',(req,res) => {
-    res.render('index')
+    res.render('index',{
+        title: 'Lecturama'
+    })
 })
 
 app.get('/libros',async (req,res) => {
