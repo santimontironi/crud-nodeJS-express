8f825f9aa0df42aacf47591264b9a8d28676db04
santimontironi <santiimontironi@gmail.com>
2025-06-27 10:58:55 -0300

    mensaje de advertencia al cerrar sesion agregado

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,10 @@
 const formEliminar = document.querySelector(".form-eliminar")
+const btnLogout = document.querySelector(".btnLogout")
 
-formEliminar.addEventListener("submit",function(e){
+btnLogout.addEventListener("click", mensajeLogout)
+formEliminar.addEventListener("submit",mensajeEliminar)
+
+function mensajeEliminar(e){
     e.preventDefault()
 
     Swal.fire({
@@ -17,4 +21,20 @@ formEliminar.addEventListener("submit",function(e){
             form.submit()
         }
     })
-})
+}
+
+function mensajeLogout(){
+    Swal.fire({
+        title: '¿Estás seguro de cerrar sesión?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Sí, cerrar sesión',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = '/logout'
+        }
+    })
+}
